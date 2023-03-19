d0b71d45d7f98d546f0e445a629ced1a77336c35
Robert Andrade <srandradejr@gmail.com>
2023-03-19 03:15:54 +0000

    Ch 12: GUI

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import Stats from 'three/examples/jsm/libs/stats.module.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -10,18 +11,19 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
-camera.position.x = 2
+camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
-const stats = Stats()
-document.body.appendChild(stats.dom)
+
 const controls = new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change', render)
+
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
-  wireframe: true
+  wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
@@ -35,11 +37,29 @@ function onWindowResize() {
   render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.open()
+
 function animate() {
   requestAnimationFrame(animate)
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+
+  //stats.begin()
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.01
+  //stats.end()
+
   render()
+
   stats.update()
 }
 
@@ -48,4 +68,4 @@ function render() {
 }
 
 animate()
-controls.addEventListener('change', render)
+//render()

9410e5e83069d89fe87bc2bf88b3b9f0dba1d0e6
Ximena Girasol <ximena.girasol@zinnia.com>
2024-02-16 04:39:17 +0000

    use path

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 
-const commonConfig = require('./webpack.common.config')
+const commonConfig = require(path.join(__dirname, 'webpack.common.config'));
 
-const productionConfig = require('./webpack.production.config');
+const productionConfig = require(path.join(__dirname, 'webpack.production.config'));
 
-const developmentConfig = require('./webpack.development.config');
+const developmentConfig = require(path.join(__dirname, 'webpack.development.config'));
 
 module.exports = (env, args) => {
   switch(args.mode) {
